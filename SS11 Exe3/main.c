31f8ffc983df6455d0bdf64651cb4cbb630a643e
mxt629 <xuantien.06021990@gmail.com>
2022-03-26 21:39:55 +0700

    Initial Commit

---
+++ SS11 Exe3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS11 Exe3
+//
+//  Created by Little Chick on 26/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
